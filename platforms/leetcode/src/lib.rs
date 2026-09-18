574af1d4d4568d748644648649416b263555e599
Keys <70819367+keys-i@users.noreply.github.com>
2026-09-18 16:12:00 +1000

    feat: expose the LeetCode client API

--- platforms/leetcode/src/lib.rs
+++ platforms/leetcode/src/lib.rs
@@ -1,1 +1,18 @@
 //! LeetCode platform crate
+
+mod client;
+mod error;
+
+pub use client::Client;
+pub use error::Error;
+
+#[derive(Debug)]
+pub struct Problem {
+    pub id: Box<str>,
+    pub title: Box<str>,
+    /// Problem statement in the HTML format returned by LeetCode
+    pub statement: Box<str>,
+}
+
+#[cfg(test)]
+mod tests;
